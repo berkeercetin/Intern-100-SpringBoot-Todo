a2e87737306859b96d0a6d9c6108b5a61c588fc0
Berke Erçetin <82932880+berkeercetin@users.noreply.github.com>
2023-11-27 15:31:17 +0300

    Initial commit

---
+++ src/main/java/com/authAngular/tutorial/authAngular/AuthAngularApplication.java
@@ -0,0 +1,13 @@
+package com.authAngular.tutorial.authAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authAngular/tutorial/authAngular/AuthAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authAngular.tutorial.authAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
